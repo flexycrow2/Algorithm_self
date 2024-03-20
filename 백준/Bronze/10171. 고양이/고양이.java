2d8a78a8656c738c221f6bf0442777794c3610e1
flexycrow2 <163443735+flexycrow2@users.noreply.github.com>
2024-03-20 17:03:11 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+public class Main {
+    /*
+    \    /\
+    )  ( ')
+    (  /  )
+     \(__)|
+     */
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
