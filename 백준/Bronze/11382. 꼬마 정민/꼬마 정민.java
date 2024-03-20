a8d65db3d5a31594a25519c03162389daa5a89ef
flexycrow2 <163443735+flexycrow2@users.noreply.github.com>
2024-03-20 16:59:00 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long A, B, C;
+        A = in.nextLong();
+        B = in.nextLong();
+        C = in.nextLong();
+        System.out.println(A + B + C);
+    }
+}
