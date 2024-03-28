8d4c3d31284bc66d247622a6d350c611365e7985
flexycrow2 <163443735+flexycrow2@users.noreply.github.com>
2024-03-28 19:01:40 +0900

    [Bronze V] Title: A+B - 7, Time: 264 ms, Memory: 18652 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int A, B;
+        int result;
+        for(int i = 1; i < T+1; i++){
+            A = sc.nextInt();
+            B = sc.nextInt();
+            result = A + B;
+            System.out.println("Case #" + i + ": " + result);
+        }
+    }
+}
